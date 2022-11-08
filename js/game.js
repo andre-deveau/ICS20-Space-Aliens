ed4262e719e8f5ab0f3e67df9c2102a356c8abbf
andre-deveau <andre-deveau@users.noreply.github.com>
2022-11-08 19:18:22 +0000

    Prettified Code!

--- js/game.js
+++ js/game.js
@@ -10,12 +10,12 @@
  * Start Phaser Game.
  */
 const config = {
-    type: Phaser.AUTO,
-    width: 1920,
-    height: 1080,
-    //set background color
-    backgroundColor: 0x5f6e7a,
-}
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  //set background color
+  backgroundColor: 0x5f6e7a,
+};
 
-const game = new Phaser.Game(config)
-console.log(game)
+const game = new Phaser.Game(config);
+console.log(game);
