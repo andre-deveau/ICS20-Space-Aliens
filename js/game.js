d80f50bb0630f4b8dfdea8cc9bf35787d46273d8
Andre <113055933+andre-deveau@users.noreply.github.com>
2022-11-10 13:59:50 -0500

    init

--- js/game.js
+++ js/game.js
@@ -9,12 +9,24 @@
 /**
  * Start Phaser Game.
  */
+//*game scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
   //set background color
   backgroundColor: 0x5f6e7a,
+  scale: {
+    mdoe: Phasers.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 };
 
 const game = new Phaser.Game(config);
