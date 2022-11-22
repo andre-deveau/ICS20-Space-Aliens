30dda090d69050bc545ff756366fb3c605f6526f
Andre <113055933+andre-deveau@users.noreply.github.com>
2022-11-22 14:02:30 -0500

    init

--- js/splashScene.js
+++ js/splashScene.js
@@ -15,6 +15,8 @@ class SplashScene extends Phaser.Scene {
    */
   constructor() {
     super({ key: "splashScene" });
+    
+    this.splashSceneBackgroundImage = null
   }
 
   /**
@@ -23,7 +25,7 @@ class SplashScene extends Phaser.Scene {
    *   before preload() and create()
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
    */
-  Infinity(data)
+  init(data)
   {
     this.cameras.main.setBackgroundColor("ffffff");
   }
@@ -35,6 +37,7 @@ class SplashScene extends Phaser.Scene {
   preload()
   {
     console.log("SplashScene");
+    this.load.image("splashSceneBackground", "./assets/splashSceneImage.png")
   }
 
   /**
@@ -42,32 +45,27 @@ class SplashScene extends Phaser.Scene {
    * Use it to creat your game objects
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
    */
-  create(data)
-  {
-    //pass
+  create(data) {
+    this.splashSceneBackgroundImage = this.add.sprite(
+      0,
+      0,
+      "splashSceneBackground"
+    )
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.x = 1080 / 2
   }
+
 /**
  * Should be overriden by your own Scenes
  * This method is called once per game step while the scene is running
  * @param {number} time - The current time
  * @param {number} delta - The delta time in ms since the last frame
  */
-update(time, delta);
-this.scene.switch("titleScene")
-{
-  // pass
-}
-
-  /**
-   * Should be overriden by your own Scenes
-   * This method is called once per game step while the scene is running
-   * @param {number} time - The current time
-   * @param {number} delta - The delta time in ms since the last frame
-   */
-  update(time, delta);
-    this.scene.switch("titleScene")
-  {
-    // pass
+update(time, delta) {
+    if (time > 3000) {
+      this.scene.switch("titleScene")
+    }
   }
 }
+
 export default SplashScene
