f92cc0f1ff054ce87c38cdc1099285b1df0511f6
Andre <113055933+andre-deveau@users.noreply.github.com>
2022-11-22 14:24:46 -0500

    bug fixes

--- js/titleScene.js
+++ js/titleScene.js
@@ -14,50 +14,45 @@ class SplashScene extends Phaser.Scene {
    * This method is the constructor
    */
   constructor() {
-    super({ key: "titleScene" });
+    super({ key: "titleScene" })
   }
-}
 
-/**
- * Can be defined on your own Scenes.
- * This method is called by the Scene Manager when the scene starts,
- *   before preload() and create()
- * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
- */
-Infinity(data);
-{
-  this.cameras.main.setBackgroundColor("ffffff");
-}
+  /**
+   * Can be defined on your own Scenes.
+   * This method is called by the Scene Manager when the scene starts,
+   *   before preload() and create()
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
+   */
+  init(data) {
+    this.cameras.main.setBackgroundColor("ffffff")
+  }
 
-/**
- * Can be defined on your own Scenes
- * Use it to load assets
- */
-preload();
-{
-  console.log("Title Scene");
-}
+  /**
+   * Can be defined on your own Scenes
+   * Use it to load assets
+   */
+  preload() {
+    console.log("Title Scene")
+  }
 
-/**
- * Can be defined on your own Scenes
- * Use it to creat your game objects
- * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
- */
-create(data);
-{
-  //pass
-}
+  /**
+   * Can be defined on your own Scenes
+   * Use it to creat your game objects
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start()
+   */
+  create(data) {
+    //pass
+  }
 
-/**
- * Should be overriden by your own Scenes
- * This method is called once per game step while the scene is running
- * @param {number} time - The current time
- * @param {number} delta - The delta time in ms since the last frame
- */
-update(time, delta);
-this.scene.switch("titleScene");
-{
-  // pass
+  /**
+   * Should be overriden by your own Scenes
+   * This method is called once per game step while the scene is running
+   * @param {number} time - The current time
+   * @param {number} delta - The delta time in ms since the last frame
+   */
+  update(time, delta) {
+    this.scene.switch("titleScene")
+  }
 }
 
 export default TitleScene;
